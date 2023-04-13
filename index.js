4ee729d55699719ab6a94c2e671d4bd0adc39d63
Ajay kumar <ajaykumarsanju25052gmail.com>
2023-04-14 00:40:56 +0530

    lil animation added

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const body = document.querySelector(".pageContent");
+const body = document.getElementById("pageContent");
 const navCheckBox = document.getElementById("navCheckBox");
+const home = document.getElementById("home");
 
 body.addEventListener("click", (evt) => {
-  if(evt.target.classList.contains("navCheckBoxLabel") || evt.target == navCheckBox || evt.target.classList.contains("themeIcon") || evt.target.classList.contains("fa-sort-down") || evt.target.classList.contains("barsIcon")) {
+  if (
+    evt.target.classList.contains("navCheckBoxLabel") ||
+    evt.target == navCheckBox ||
+    evt.target.classList.contains("themeIcon") ||
+    evt.target.classList.contains("fa-sort-down") ||
+    evt.target.classList.contains("barsIcon")
+  ) {
     return;
   }
   console.log(evt.target);
   navCheckBox.checked = false;
-})
+});
 
-body.addEventListener('scroll', function() {
-   if(body.scrollTop >= 200) {
-     document.querySelector('.topIcon').classList.add('showToTopIcon');
-   } else {
-     document.querySelector('.topIcon').classList.remove('showToTopIcon');
-   }
- });
+body.addEventListener("scroll", function () {
+  const rect = document.querySelector("#home").getBoundingClientRect();
+  const bodyRect = body.getBoundingClientRect();
+  const visible = rect.top >= bodyRect.top && rect.bottom <= bodyRect.bottom;
+  if (visible) {
+    document.querySelector("#home div").classList.add("animateFromRight");
+    document.querySelector("#home img").classList.add("animateFromLeft");
+  } else {
+    document.querySelector("#home div").classList.remove("animateFromRight");
+    document.querySelector("#home img").classList.remove("animateFromLeft");
+  }
+
+  if (body.scrollTop >= 200) {
+    document.querySelector(".topIcon").classList.add("showToTopIcon");
+  } else {
+    document.querySelector(".topIcon").classList.remove("showToTopIcon");
+  }
+});
