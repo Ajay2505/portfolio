e69a018450bd639bfaec33df6757e3b1d2497023
Ajay kumar <ajaykumarsanju25052gmail.com>
2023-04-20 00:26:32 +0530

    done carousel animated some stuff ig

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const body = document.getElementById("pageContent");
 const navCheckBox = document.getElementById("navCheckBox");
 const home = document.getElementById("home");
+
 const carousel = document.getElementById("carousel");
 const carouselTitle = document.getElementById("carouselTitle");
 const leftArrow = document.getElementById("leftArrow");
 const rightArrow = document.getElementById("rightArrow");
 const carouselItems = document.querySelectorAll("#carousel img");
 let currIndex = 1;
+const carouselTitles = [
+  "TokoGadget",
+  "Flash Chat",
+  "Password Manager",
+  "Portfolio",
+  "ChatApp",
+  "Password Manager",
+];
+
+carousel.addEventListener('touchstart', function(e) {
+  var touchStartX = e.touches[0].clientX;
+  carousel.addEventListener('touchmove', function(e) {
+    var touchMoveX = e.touches[0].clientX;
+    var swipeDistance = touchMoveX - touchStartX;
+    if (swipeDistance > 0) {
+      rightCar();
+    } else {
+      leftCar();
+    }
+  });
+});
 
 body.addEventListener("click", (evt) => {
   if (
@@ -18,20 +40,20 @@ body.addEventListener("click", (evt) => {
   ) {
     return;
   }
-  
+
   navCheckBox.checked = false;
 });
 
 body.addEventListener("scroll", function () {
   const homeRect = document.querySelector("#home").getBoundingClientRect();
-  const works = document.querySelectorAll("#work div.work");
-  
+  const elements = document.querySelectorAll(".scrollAnimation");
+
   if (body.scrollTop > 200) {
     document.querySelector(".topIcon").classList.add("showToTopIcon");
   } else {
     document.querySelector(".topIcon").classList.remove("showToTopIcon");
   }
-  
+
   if (homeRect.bottom > 229) {
     document.querySelector("#home div").classList.add("animateFromRight");
     document.querySelector("#home img").classList.add("animateFromLeft");
@@ -40,11 +62,11 @@ body.addEventListener("scroll", function () {
     document.querySelector("#home img").classList.remove("animateFromLeft");
   }
 
-  for(let i = 0; i < works.length; i++) {
-    if (isElementVisible(works[i], body)) {
-      works[i].classList.add("animateFromLeft");
+  for (let i = 0; i < elements.length; i++) {
+    if (isElementVisible(elements[i], body)) {
+      elements[i].classList.add("animateFromLeft");
     } else {
-      works[i].classList.remove("animateFromLeft");
+      elements[i].classList.remove("animateFromLeft");
     }
   }
 });
@@ -52,65 +74,58 @@ body.addEventListener("scroll", function () {
 const isElementVisible = (element, container) => {
   var rect = element.getBoundingClientRect();
   var containerRect = container.getBoundingClientRect();
-  var topVisible = rect.top >= containerRect.top && rect.top <= containerRect.bottom * 0.8;
-  var bottomVisible = rect.bottom <= containerRect.bottom && rect.bottom >= containerRect.top * 1.2;
+  var topVisible = rect.top >= containerRect.top && rect.top <= containerRect.bottom * 0.9;
+  var bottomVisible = rect.bottom <= containerRect.bottom && rect.bottom >= containerRect.top * 1.1;
   return topVisible || bottomVisible;
-}
-
-const carouselTitles = [
-  "TokoGadget",
-  "Flash Chat",
-  "Password Manager",
-  "Portfolio",
-  "Flash Chat",
-  "Password Manager",
-];
-
-const leftCar = () => {
-  if (currIndex + 1 >= carouselItems.length) return;
-
-  currIndex++;
-  carouselTitle.innerText = carouselTitles[currIndex];
-  rightArrow.classList.remove("hide");
-  
-  if(currIndex-2 >= 0) {
-    carouselItems[currIndex-2].className = "";
-    carouselItems[currIndex-2].classList.add("hide");
-  }
-  carouselItems[currIndex].className = "";
-  carouselItems[currIndex].classList.add("moveCenter");
-  carouselItems[currIndex-1].className = "";
-  carouselItems[currIndex-1].classList.add("moveLeft");
-
-  if(currIndex + 1 >= carouselItems.length) {
-    leftArrow.classList.add("hide");
-  } else {
-    carouselItems[currIndex+1].className = "";
-    carouselItems[currIndex+1].classList.add("placeRight");
-  }
-
-}
+};
 
 const rightCar = () => {
   if (currIndex - 1 < 0) return;
+  leftArrow.classList.remove("hide");
+
+  if (currIndex + 1 < carouselItems.length) {
+    carouselItems[currIndex + 1].className = "";
+    carouselItems[currIndex + 1].classList.add("hide");
+  }
+
+  carouselItems[currIndex].className = "";
+  carouselItems[currIndex - 1].className = "";
+  carouselItems[currIndex].classList.add("placeRight");
+  carouselItems[currIndex - 1].classList.add("placeCenter");
+  carouselTitle.innerText = carouselTitles[currIndex - 1];
 
   currIndex--;
-  carouselTitle.innerText = carouselTitles[currIndex];
-  leftArrow.classList.remove("hide");
-  
-  if(currIndex+2 < carouselItems.length) {
-    carouselItems[currIndex+2].className = "";
-    carouselItems[currIndex+2].classList.add("hide");
-  }
-  carouselItems[currIndex+1].className = "";
-  carouselItems[currIndex+1].classList.add("placeRight");
-  carouselItems[currIndex].className = "";
-  carouselItems[currIndex].classList.add("moveCenterFromLeft");
 
-  if(currIndex - 1 < 0) {
+  if (currIndex <= 0) {
     rightArrow.classList.add("hide");
   } else {
-    carouselItems[currIndex-1].className = "";
-    carouselItems[currIndex-1].classList.add("placeLeft");
+    carouselItems[currIndex - 1].className = "";
+    carouselItems[currIndex - 1].classList.add("placeLeft");
   }
-}
+};
+
+const leftCar = () => {
+  if (currIndex + 1 >= carouselItems.length) return;
+  rightArrow.classList.remove("hide");
+
+  if (currIndex - 1 >= 0) {
+    carouselItems[currIndex - 1].className = "";
+    carouselItems[currIndex - 1].classList.add("hide");
+  }
+
+  carouselItems[currIndex].className = "";
+  carouselItems[currIndex].classList.add("placeLeft");
+
+  carouselTitle.innerText = carouselTitles[currIndex + 1];
+  carouselItems[currIndex + 1].className = "";
+  carouselItems[currIndex + 1].classList.add("placeCenter");
+
+  currIndex++;
+
+  if (currIndex + 1 >= carouselItems.length) {
+    leftArrow.classList.add("hide");
+  } else {
+    carouselItems[currIndex + 1].className = "";
+    carouselItems[currIndex + 1].classList.add("placeRight");
+  }
+};
